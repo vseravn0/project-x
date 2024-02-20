b8851ad86d4d247572a71ccb2bffadacafbf7dd5
vseravno <vseravno2019@gmail.com>
2024-02-20 19:50:57 +0700

    feat(): added storybook

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import { buildCssLoader } from './loaders/buildCssLoader';
 
 export const buildLoaders = ({ isDev }:BuildOptions):webpack.RuleSetRule[] => {
     const typescriptLoader = {
@@ -9,22 +9,7 @@ export const buildLoaders = ({ isDev }:BuildOptions):webpack.RuleSetRule[] => {
         exclude: /node_modules/,
     };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath:string) => resPath.includes('.module.'),
-                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
-                    },
-                },
-            },
-            'sass-loader',
-        ],
-    };
+    const cssLoader = buildCssLoader(isDev);
 
     const svgLoader = {
         test: /\.svg$/,
---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import { addDecorator } from '@storybook/react';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
+addDecorator(RouterDecorator);
